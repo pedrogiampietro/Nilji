5c28fa0ac6d103ec445cafba08a421373305236e
Pedro Giampietro <pedrogiampietro@hotmail.com>
2020-08-12 19:45:41 -0300

    Terminando pagina de Produtos, estilizando paginas.

--- database.sql
+++ database.sql
@@ -2,25 +2,25 @@ CREATE TABLE "products" (
   "id" SERIAL PRIMARY KEY,
   "category_id" int UNIQUE,
   "user_id" int UNIQUE,
-  "name" text,
-  "description" text,
+  "name" text NOT NULL,
+  "description" text NOT NULL,
   "old_price" int,
-  "price" int,
-  "quantity" int,
-  "status" int,
+  "price" int NOT NULL,
+  "quantity" int DEFAULT 0,
+  "status" int DEFAULT 1,
   "created_at" timestamp DEFAULT (now()),
   "updated_at" timestamp DEFAULT (now())
 );
 
 CREATE TABLE "categories" (
   "id" SERIAL PRIMARY KEY,
-  "name" text
+  "name" text NOT NULL
 );
 
 CREATE TABLE "files" (
   "id" SERIAL PRIMARY KEY,
-  "name" text,
-  "path" text,
+  "name" text NOT NULL,
+  "path" text NOT NULL,
   "product_id" int UNIQUE
 );
 
