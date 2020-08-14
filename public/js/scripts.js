344c3ce6f8a4d1489c30ca99a50fd61c3ed5daa1
Pedro Giampietro <pedrogiampietro@hotmail.com>
2020-08-14 19:56:08 -0300

    Refatorando código, utilizando estrategia de máscara.

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,9 +1,15 @@
-const input = document.querySelector('input[name="price"]')
-input.addEventListener('keydown', (e) => {
-    
-    setTimeout(() => {
-        let { value } = e.target
+const Mask = {
+    apply(input, func) {
+        setTimeout(() => {
+            input.value = Mask[func](input.value)
+        }, 1)
+    }, 
+    formatBRL(value) {
         value = value.replace(/\D/g, '')
-        e.target.value = value
-    })
-})
+        
+        return new Intl.NumberFormat('pt-BR', {
+            style: 'currency',
+            currency: 'BRL'
+        }).format(value / 100)
+    }
+}
