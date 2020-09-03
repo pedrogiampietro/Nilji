21508801e08ef3f47893a180b8a4494ca27bfa78
Pedro Giampietro <pedrogiampietro@hotmail.com>
2020-09-03 20:33:16 -0300

    Adicionado Seach, e filtro com categoria.

--- src/routes.js
+++ src/routes.js
@@ -2,13 +2,17 @@ const express = require('express')
 const routes = express.Router()
 const multer = require('./app/middlewares/multer')
 
-const ProductController = require('./app/controllers/ProductController')
 const HomeController = require('./app/controllers/HomeController')
+const ProductController = require('./app/controllers/ProductController')
+const SearchController = require('./app/controllers/SearchController')
 
-
+// Home
 routes.get('/', HomeController.index)
 
+// Search
+routes.get('/products/search', SearchController.index)
 
+// Products
 routes.get('/products/create', ProductController.create)
 routes.get('/products/:id', ProductController.show)
 routes.get('/products/:id/edit', ProductController.edit)
